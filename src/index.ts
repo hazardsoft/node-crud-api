38b0144f588d6d5570040b0418825eacf1dd389f
Henadzi Shutko <hazardsoft@gmail.com>
2025-05-12 23:05:04 +0300

    feat: add dummy http server

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,16 @@
-export function main() {
-  return "Hello World";
-}
+import http from "node:http";
 
-console.log(main());
+const port = 4000;
+
+const server = http.createServer(async (req, res) => {
+  console.log(`received request via ${req.method} method`);
+  res.writeHead(200);
+  res.end(JSON.stringify({
+    status: 200,
+    message: 'Ok'
+  }));
+})
+
+server.listen(port, () => {
+  console.log(`server is running at port ${port}`)
+});
